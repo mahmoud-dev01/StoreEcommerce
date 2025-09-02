97e4da288e90d2eadd52bb3e95778fe37f5dc5d7
Mahmoud Ghanem <mahmoud.dev01@gmail.com>
2025-09-02 07:33:27 +0300

    add animation splash screen

--- app/src/main/java/com/example/storeecommerce/MainActivity.kt
+++ app/src/main/java/com/example/storeecommerce/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.storeecommerce
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,10 +15,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.example.storeecommerce.ui.theme.StoreEcommerceTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        initSetupSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -25,6 +35,36 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+            }
+        }
+    }
+    private fun initSetupSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashscreen = installSplashScreen()
+            var keepSplashScreen = true
+            splashscreen.setKeepOnScreenCondition { keepSplashScreen }
+            lifecycleScope.launch {
+                delay(5000)
+                keepSplashScreen = false
+            }
+            // Add a callback that's called when the splash screen is animating to the
+            // app content.
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Create your custom animation.
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 200L
+
+                // Call SplashScreenView.remove at the end of your custom animation.
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                // Run your animation.
+                slideUp.start()
             }
         }
     }
